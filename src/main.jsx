c3b26beccd7f3bad5635f9e3ba6e7f49966ad927
Ibnu Putra <azxarblog@gmail.com>
2023-12-18 22:58:24 +0700

    added basename at routes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <ThemeProvider>
         <App />
       </ThemeProvider>
